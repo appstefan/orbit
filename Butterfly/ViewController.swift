bac2b3f64dd48be35015d3ba3210765f339f7577
Stefan Britton <stefan.britton@me.com>
2017-03-10 17:07:53 -0600

    Initial Commit

---
+++ Butterfly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Butterfly
+//
+//  Created by Stefan Britton on 2017-03-10.
+//  Copyright © 2017 Kasama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
